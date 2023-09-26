c7c0cf8353c5c1b1dbbb1b32254d6e639551a0a3
DoraSoares <soaresmariaauxiliadora@gmail.com>
2023-09-26 17:55:04 -0300

    versãofinalcarros

---
+++ listaCarros.js
@@ -0,0 +1,32 @@
+let listaCarros =  [
+    {
+        "nome": "Drako GTE",
+        "img": "img/drakogte.jpg",
+        "descricao": "O Drako GTE é um sedã esportivo elétrico de luxo fabricado pela Drako Motors. O GTE é baseado no modelo Karma Revero GT.",
+    },        
+    {
+        "nome": "De Tomaso",
+        "img": "img/deTomaso.jpg",
+        "descricao": "O De Tomaso Pantera é um carro esportivo de motor central produzido pela fabricante italiana de automóveis De Tomaso de 1971 a 1992.  O Pantera foi o modelo mais popular da montadora, com mais de 7.000 fabricados ao longo de sua produção de vinte anos.",
+    },
+    {
+        "nome": "Ferrari Laferrari",
+        "img": "img/Ferrari.jpg",
+        "descricao": "LaFerrari, nome de projeto F150, é um motor central de produção limitada, carro esportivo híbrido leve construído pela fabricante italiana de automóveis Ferrari. LaFerrari significa A Ferrari em italiano, o que significa que esta deve ser a Ferrari definitiva. LaFerrari baseia-se nos resultados dos testes do protótipo de desenvolvimento FXX.",
+    },
+    {
+        "nome": "MacLaren",
+        "img": "img/maclaren.jpg",
+        "descricao": "O McLaren Elva é um carro esportivo de motor central de produção limitada fabricado pela McLaren Automotive. O carro é a quinta adição na McLaren Ultimate Series, juntando-se à F1, P1, Senna e Speedtail. O carro esportivo de topo aberto é inspirado nos carros de corrida de topo aberto desenvolvidos por Bruce McLaren na década de 1960",
+    },
+    {
+        "nome": "Pagani",
+        "img": "img/pagani.jpg",
+        "descricao": "O Pagani Huayra é um carro esportivo de motor central produzido pela fabricante italiana de carros esportivos Pagani, sucedendo a oferta anterior da empresa, o Zonda. Seu nome é uma homenagem a Wayra Tata, o deus do vento quíchua (aborígene sul-americano). O Huayra tem uma velocidade máxima de cerca de 383 km/h (238 mph) e tem um tempo de aceleração de 0-97 km/h (60 mph) de 2,8 segundos.",
+    },
+    {
+        "nome": "Porsche 718",
+        "img": "img/porsche718.jpg",
+        "descricao": "O Porsche 718 é uma série de carros esportivos de um ou dois lugares construídos pela Porsche de 1957 a 1962. Um modelo monoposto de roda aberta foi desenvolvido para corridas de Fórmula. O 718 foi um desenvolvimento do bem-sucedido Porsche 550A com melhorias feitas no trabalho da carroceria e suspensão. O nome completo do carro é 718 RSK, onde RS significa RennSport (corridas esportivas) e o K reflete o formato da suspensão revisada da barra de torção do carro.",
+    }
+]
